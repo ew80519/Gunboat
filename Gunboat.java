c6824f778c8f2f3bbbe0097e8df52cad641c8035
ew80519 <69615151+ew80519@users.noreply.github.com>
2020-08-17 14:30:25 -0700

    Update Gunboat.java

--- Gunboat.java
+++ Gunboat.java
@@ -5,21 +5,21 @@ public class Main
 
 	public static void main(String[] args) {
     
-        	Scanner reader = new Scanner(System.in);
-        	System.out.println("Enter distance to target.\n");
-        	double d = reader.nextDouble();
-        	System.out.println("Enter the height of the battery relative to the water line.\n");
-        	double h = reader.nextDouble();
-        	System.out.println("Enter the initial velocity of the projectile.\n");
-        	double v = reader.nextDouble();
+        Scanner reader = new Scanner(System.in);
+        System.out.println("Enter distance to target.\n");
+        double d = reader.nextDouble();
+        System.out.println("Enter the height of the battery relative to the water line.\n");
+        double h = reader.nextDouble();
+        System.out.println("Enter the initial velocity of the projectile.\n");
+        double v = reader.nextDouble();
 		
 		double a = -4.9 * (d/v) * (d/v);
 		double b = d;
-        	double c = a + h;	
+        double c = a + h;	
         
-        	double tanAngle = (-b + Math.sqrt(b * b - 4 * a * c)) / (2 * a);
-        	double Angle = Math.atan(tanAngle) * (180/3.14159265359);
-        	System.out.println("Angle of cannon: " + Angle);
+        double tanAngle = (-b + Math.sqrt(b * b - 4 * a * c)) / (2 * a);
+        double Angle = Math.atan(tanAngle) * (180/3.14159265359);
+        System.out.println("Angle of cannon: " + Angle);
 		
 	}
 }
